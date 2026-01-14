7462fb3a92f4641f6b7995fe13bd16a6497b56f4
sanjib001 <sanjibpandey94@gmail.com>
2026-01-14 18:14:36 +0545

    Adds form uing state component

--- my-react-app/src/components/input.jsx
+++ my-react-app/src/components/input.jsx
@@ -1,19 +1,43 @@
 import { useState } from "react";
 
 const Input = () => {
-    const [username, setUsername] = useState("");
+    const [state, setState] = useState({
+        username: "",
+        email: "",
+        password: "",
+        phone: "",
+    })
 
     const handleChange = (event) => {
-        console.log(event.target.value)
-        setUsername(event.target.value)
+        const name = event.target.name;
+        const value = event.target.value;
+        setState({ ...state, [name]: value })
     }
+    console.log(state);
 
     return (
         <>
-            <label>Username: </label>
-            <input onChange={handleChange} type="text" placeholder="Enter Your Username" />
             <div>
-                Username typed: {username}
+                <label>Username: </label>
+                <input onChange={handleChange} name="username" type="text" placeholder="Enter Your Username" />
+            </div>
+             <div>
+                <label>Email: </label>
+                <input onChange={handleChange} name="email" type="email" placeholder="Enter Your email" />
+            </div>
+             <div>
+                <label>Password: </label>
+                <input onChange={handleChange} name="password" type="text" placeholder="Enter Your Password" />
+            </div>
+             <div>
+                <label>Phone: </label>
+                <input onChange={handleChange} name="phone" type="text" placeholder="Enter Your phone" />
+            </div>
+            <div>
+                Username : {state.username}<br/>
+                emial : {state.email}<br/>
+                Password : {state.password}<br/>
+                phone: {state.phone}
             </div>
         </>
     )
